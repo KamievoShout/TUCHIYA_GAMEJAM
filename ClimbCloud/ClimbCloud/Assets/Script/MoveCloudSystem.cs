ab7dc8286348d7b59b4b2d4747e7a8c05747ee68
IsooGabriel <139324502+IsooGabriel@users.noreply.github.com>
2023-07-12 17:47:37 +0900

    無害
    ころしてくれ

--- ClimbCloud/ClimbCloud/Assets/Script/MoveCloudSystem.cs
+++ ClimbCloud/ClimbCloud/Assets/Script/MoveCloudSystem.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class oaho : MonoBehaviour
+public class MoveCloudSystem : MonoBehaviour
 {
+    [Seliazi]
+
     // Start is called before the first frame update
     void Start()
     {
