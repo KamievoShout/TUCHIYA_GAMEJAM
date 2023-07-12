fdeec1a3347bb1bfb5f9e79cda141840315f9b10
nkc20236039 <nkc23.kawai.yuto@gmail.com>
2023-07-12 17:29:50 +0900

    Add: Unity project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject player;
+
+    void Start()
+    {
+        this.player = GameObject.Find("cat");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = this.player.transform.position;
+        transform.position = new Vector3(transform.position.x, playerPos.y, transform.position.z);
+    }
+}
