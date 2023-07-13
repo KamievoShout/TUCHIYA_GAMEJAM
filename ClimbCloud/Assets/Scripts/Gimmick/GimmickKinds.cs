a725ba0b5439c1fe65d563c45a46adf2a2157640
ayaha401 <oshizawadai.1192@gmail.com>
2023-07-13 14:08:35 +0900

    ギミックの種類を表すEnumを作成

---
+++ ClimbCloud/Assets/Scripts/Gimmick/GimmickKinds.cs
@@ -0,0 +1,11 @@
+/// <summary>
+/// ギミックの種類
+/// </summary>
+public enum GimmickKinds
+{
+    MoveReverse = 0,    // 入力を反転
+    Buff = 1,           // バフをあたえる
+    MoveLift = 2,       // 動く床を出す
+    BlackOut = 3,       // 画面を暗くする
+    Pillar = 4          // 柱を出す
+}
