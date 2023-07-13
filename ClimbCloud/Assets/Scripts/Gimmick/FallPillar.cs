182b2f45df1a1d07fd08d9be23117bc6f0d25cae
nkc-iwamoto <ayato20030213@icloud.com>
2023-07-13 17:07:01 +0900

    add:柱が落ちてくるプレハブを追加、プレイヤーと柱のレイヤーを追加、当たり判定のレイヤーを設定

--- ClimbCloud/Assets/Scripts/Gimmick/FallPillar.cs
+++ ClimbCloud/Assets/Scripts/Gimmick/FallPillar.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class FallPillar : Gimmick
@@ -7,15 +8,28 @@ public class FallPillar : Gimmick
     [SerializeField, Header("柱のオブジェクト")]
     private GameObject _pillar;
 
-    [SerializeField, Header("プレイヤーのTransform")]
-    private Transform _playerTransform;
+    [SerializeField, Header("プレイヤーから離す距離")]
+    private Vector2 _diffPosition;
+
+     // テスト用のプレイヤー座標
+   　// public Transform t;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Instantiate(_pillar, new Vector3(0, 0, 0), Quaternion.identity);
-        }
+        //if (Input.GetKeyDown(KeyCode.Escape))
+        //{
+        //    GeneratePillar(t.position);
+        //}
+    }
+
+    /// <summary>
+    /// 柱を生成する
+    /// </summary>
+    /// <param name="playerPos">プレイヤーの現在座標</param>
+    public void GeneratePillar(Vector2 playerPos)
+    {
+        Vector2 generatePos = playerPos + _diffPosition;
+        Instantiate(_pillar, generatePos, Quaternion.identity);
     }
 
 }
