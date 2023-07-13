09120d649c904b56b2b7de464f9a9a407dab5974
ayaha401 <oshizawadai.1192@gmail.com>
2023-07-14 02:25:58 +0900

    プレイヤーにどんなデバフが付与されたのかわかるようになった

--- ClimbCloud/Assets/Scripts/UI/DisplayPlayerDebuff.cs
+++ ClimbCloud/Assets/Scripts/UI/DisplayPlayerDebuff.cs
@@ -11,10 +11,30 @@ public class DisplayPlayerDebuff : MonoBehaviour
     [SerializeField]
     private Image buffUi;
 
+    private GameObject rootObj;
+
     void Start()
     {
         moveReverseUi.enabled = false;
         buffUi.enabled = false;
+
+        rootObj = gameObject.transform.root.gameObject;
+    }
+
+    private void Update()
+    {
+        if (rootObj.transform.localScale.x < 0)
+        {
+            Vector3 localScale = this.gameObject.transform.localScale;
+            localScale.x = -1;
+            this.gameObject.transform.localScale = localScale;
+        }
+        else
+        {
+            Vector3 localScale = this.gameObject.transform.localScale;
+            localScale.x = 1;
+            this.gameObject.transform.localScale = localScale;
+        }
     }
 
     /// <summary>
