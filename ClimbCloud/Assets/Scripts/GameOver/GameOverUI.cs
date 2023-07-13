a74384e5374bb5dee4e9c59fab6b219a3339128f
kmamonnassi <ka82573@jk2.so-net.ne.jp>
2023-07-14 02:30:00 +0900

    ゲームオーバー時に記録を出すように

--- ClimbCloud/Assets/Scripts/GameOver/GameOverUI.cs
+++ ClimbCloud/Assets/Scripts/GameOver/GameOverUI.cs
@@ -1,12 +1,30 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverUI : MonoBehaviour
 {
-    public void Active()
+    [SerializeField] private Image[] numberImages;
+    [SerializeField] private Sprite[] numberSprites;
+
+    public void Active(int maxPos)
     {
         gameObject.SetActive(true);
-	}
+
+        int normalize = maxPos / 32;
+        Debug.Log(normalize);
+        int digits = normalize.ToString().Length;
+        for(int i = 0; i < numberImages.Length;i++)
+        {
+            bool isActive = i <= digits - 1;
+            numberImages[i].gameObject.SetActive(isActive);
+            if (isActive)
+            {
+                int num = int.Parse(normalize.ToString()[i].ToString());
+                numberImages[i].sprite = numberSprites[num];
+            }
+        }
+    }
 
     public void Retry()
     {
--- ClimbCloud/Assets/Scripts/Other/GameOver.cs
+++ ClimbCloud/Assets/Scripts/Other/GameOver.cs
@@ -5,9 +5,23 @@ using Utility;
 public class GameOver : MonoBehaviour
 {
     [SerializeField] private GameOverUI gameOverUI;
+
+    private Transform playerTra;
+    private float maxPos;
+
     private void Start()
     {
         PlayerController ctrl = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
-        ctrl.OnDead += gameOverUI.Active;
+        ctrl.OnDead += () => gameOverUI.Active(Mathf.CeilToInt(maxPos));
+        playerTra = GameObject.FindWithTag("Player").transform;
     }
+
+	private void Update()
+	{
+        if (playerTra == null) return;
+        if(playerTra.position.y > maxPos)
+        {
+            maxPos = playerTra.position.y;
+        }
+	}
 }
