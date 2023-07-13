431269193820395f19279d520c2919679ebe4291
Takeuchi <yoribaseball623@gmail.com>
2023-07-13 16:01:30 +0900

    自分のシーンの追加

--- ClimbCloud/Assets/Takeuchi/Cloud/Cloud.cs
+++ ClimbCloud/Assets/Takeuchi/Cloud/Cloud.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Cloud : MonoBehaviour
 {
-
+    BoxCollider2D box2D;   
     // Start is called before the first frame update
     void Start()
+    {
+        box2D = this.GetComponent<BoxCollider2D>();
+    }
+
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.tag=="Player")
+        {
+
+            //this.transform.position
+        }
     }
 }
