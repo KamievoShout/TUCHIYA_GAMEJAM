942357f8f68afb30d209fe7c19d6f02ea9985838
nakamura <nkc.daichi.nakamura@gmail.com>
2023-07-13 16:28:42 +0900

    猫のアニメーションを実装しました

---
+++ CloudClimber/Assets/Scripts/Game/Player/PlayerAnimation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+namespace PlayerInternal
+{
+    public class PlayerAnimation : MonoBehaviour
+    {
+        [SerializeField]
+        Animator animator;
+
+        private void Awake()
+        {
+            var mover = GetComponent<PlayerMover>();
+            mover.Jumping
+            .Subscribe(_ =>
+            {
+                animator.SetTrigger("JumpTrigger");
+            })
+            .AddTo(this);
+
+            mover.Speed
+                .Subscribe(value =>
+                {
+                    animator.speed = Mathf.Abs(value);
+                }).AddTo(this);
+
+            mover.Speed
+                .Where(speed => speed != 0.0f)
+                .Subscribe(speed =>
+                {
+                    float rot = speed > 0 ? 0.0f : 180.0f;
+                    animator.transform.localEulerAngles = new Vector3(0.0f, rot, 0.0f);
+                })
+                .AddTo(this);
+        }
+    }
+}
