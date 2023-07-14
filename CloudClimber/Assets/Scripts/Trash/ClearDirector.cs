de026315abe1bcc51e989c18fcf37f82d998f3bb
nkc20236112 <nkc23.serigano.masato@gmail.com>
2023-07-14 16:44:01 +0900

    ゲームクリアとゲームオーバーシーンにアニメーション追加

--- CloudClimber/Assets/Scripts/Trash/ClearDirector.cs
+++ CloudClimber/Assets/Scripts/Trash/ClearDirector.cs
@@ -2,14 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;  // LoadSceneを使うために必要！！
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class ClearDirector : MonoBehaviour
 {
-    void Update()
+    [SerializeField]
+    GameData Data;
+    [SerializeField]
+    GameObject GameClear;
+    [SerializeField]
+    GameObject GameOver;
+    [SerializeField]
+    Text text;
+    RectTransform rectTransform;
+
+void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        text.text = Data.TimeLeft.ToString("Time: "+ Data.TimeLeft);
+
+        if (Data.IsCleared)
+        {
+            GameClear.SetActive(true);
+            GameOver.SetActive(false);
+        }
+        if (Data.TimeLeft < 0)
+        {
+            GameOver.SetActive(true);
+            GameClear.SetActive(false);
+        }
+
+        if (Input.GetKey(KeyCode.R))
         {
             SceneManager.LoadScene("GameScene");
         }
+        //if(Input.GetKey(KeyCode.T))
+        //{
+        //    SceneManager.LoadScene("TitleScene");
+        //}
     }
 }
--- CloudClimber/Assets/TextController.cs
+++ CloudClimber/Assets/TextController.cs
@@ -1,18 +1,27 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextController : MonoBehaviour
 {
+    public Text GameOverText;
+    public Text GameClearText;
+    public Text TimescoreText;
+
     void Awake()
     {
-        transform.localScale = Vector3.zero;
+        //transform.localScale = Vector3.zero;
         ShowWindow();
     }
 
     void ShowWindow()
     {
+        GameOverText.GetComponent<RectTransform>().DOAnchorPos(new Vector2(0, 0),1f).SetEase(Ease.OutBounce);
+        GameClearText.GetComponent<RectTransform>().DOAnchorPos(new Vector2(0, 0), 1f).SetEase(Ease.OutBounce);
+        TimescoreText.GetComponent<RectTransform>().DOAnchorPos(new Vector2(0, -60), 0.5f).SetDelay(1f);
         transform.DOScale(1f, 1f).SetEase(Ease.OutBounce);
     }
 }
