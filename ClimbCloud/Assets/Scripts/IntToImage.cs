0d0b57dd3f323882e6f26fea0138270fccf1121b
ayaha401 <oshizawadai.1192@gmail.com>
2023-07-14 11:40:22 +0900

    UIのバグを修正

--- ClimbCloud/Assets/Scripts/IntToImage.cs
+++ ClimbCloud/Assets/Scripts/IntToImage.cs
@@ -14,30 +14,14 @@ public class IntToImage : MonoBehaviour
     /// <param name="digits">桁数</param>
     public Sprite[] GetSprites(int num, int digits)
     {
-        int numDisits = num.ToString().Length;
-        int disitsDiff = Mathf.Abs(digits - numDisits);
-
+        string numStr = num.ToString().PadLeft(digits, '0');
         Sprite[] sprites = new Sprite[digits];
-        for (int i = 1; i <= digits; i++)
+        for (int i = 0; i < digits; i++)
         {
-            if(disitsDiff > 0 && (i + 1) <= disitsDiff)
-            {
-                sprites[i - 1] = numSpriteList[0];
-                continue;
-            }
-            int digitNum = GetPointDigit(num, i);
-            sprites[i - 1] = numSpriteList[digitNum];
+            char c = numStr[i];
+            int n = int.Parse(c.ToString());
+            sprites[i] = numSpriteList[n];
         }
         return sprites;
-    }
-
-    /// <summary>
-    /// 指定した桁の数字を取り出す
-    /// </summary>
-    /// <param name="num">数値</param>
-    /// <param name="digit">取り出したい桁</param>
-    private int GetPointDigit(int num, int digit)
-    {
-        return (int)(num / Mathf.Pow(10, digit - 1)) % 10;
     }
 }
